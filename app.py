54562f72bb3074f1f402ea020a2584d3e7245da7
zheqiaochen <chenzheqiao@hotmail.com>
2025-01-13 00:07:26 -0500

    vercel

--- app.py
+++ app.py
@@ -159,7 +159,3 @@ async def extract_jsonl(jsonl_file: UploadFile):
             
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8080)
